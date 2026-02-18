f2e5ae22b311630e2eb88e2db3311e8e540820ce
Kevin-Berardi-5977 <kpb5977@psu.edu>
2026-02-18 08:54:16 -0500

    SDPM Code Vectorized
    All for loops replaced with vectorized equivalents to increase computational efficiency,
    
    Functionality has been verified prior to commit

--- SDPM_Execute.m
+++ SDPM_Execute.m
@@ -9,7 +9,7 @@
 clc
 clear
 %% ========== INPUT PARAMETERS ========= %
-alphaD = 10; % Angle of attack (degrees)
+alphaD = 5; % Angle of attack (degrees)
 U = 1; % Free stream velocity magnitude
 c = 1; % Chord Length (m)
 m = 1.48E-5; % kinematic viscosity of air at 15 degrees C
@@ -32,17 +32,17 @@ result = Dirichilet_ConstantSourceDoublet(Bp, alphaD, U)
 % READ LABELS ABOVE FIGURE PLOTS
 
 % For alphaD = 10 use this portion and comment out ExpData3 and figure(2)
-figure(1); hold on;
-set(gca, 'YDir','reverse')
-plot(result.X_Cp, result.Cp, '-b')
-plot(ExpData1(:,1), ExpData1(:,2), 'r*')
-plot(ExpData2(:,1), ExpData2(:,2), 'go')
-title(['Pressure Distribution on Airfoil Surface ($\alpha = ', num2str(alphaD), ')$'], 'Interpreter','latex');
-xlabel('X-Coordinate of Airfoil');
-ylabel('Coefficient of Pressure (Cp)');
-legend('Berardi SDPM Code', 'Gregory, Re = 3 \times 10^7', 'Ladson, Re = 6 /times 10^7', 'Katz & Plotkin SDPM Code', Location='northeast')
-axis padded
-hold off
+% figure(1); hold on;
+% set(gca, 'YDir','reverse')
+% plot(result.X_Cp, result.Cp, '-b')
+% plot(ExpData1(:,1), ExpData1(:,2), 'r*')
+% plot(ExpData2(:,1), ExpData2(:,2), 'go')
+% title(['Pressure Distribution on Airfoil Surface ($\alpha = ', num2str(alphaD), ')$'], 'Interpreter','latex');
+% xlabel('X-Coordinate of Airfoil');
+% ylabel('Coefficient of Pressure (Cp)');
+% legend('Berardi SDPM Code', 'Gregory, Re = 3 \times 10^7', 'Ladson, Re = 6 /times 10^7', 'Katz & Plotkin SDPM Code', Location='northeast')
+% axis padded
+% hold off
 
 % For alphaD = 5 use this portion and comment out ExpData1, ExpData2, and figure(1)
 figure(2); hold on;
@@ -52,6 +52,6 @@ plot(ExpData3(:,1), ExpData3(:,2), '^k')
 title(['Pressure Distribution on Airfoil Surface ($\alpha = ', num2str(alphaD), ')$'], 'Interpreter','latex');
 xlabel('X-Coordinate of Airfoil');
 ylabel('Coefficient of Pressure (Cp)');
-legend('Berardi SDPM Code', 'Katz and Plotkin SDPM Code', Location='northeast'
+legend('Berardi SDPM Code', 'Katz and Plotkin SDPM Code', Location='northeast')
 axis padded
 hold off
